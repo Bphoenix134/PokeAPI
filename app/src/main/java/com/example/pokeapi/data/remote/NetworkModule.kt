05f2d89fc667c2d62272d8fc7d31bab10a364955
Kris <blackphoenix134@mail.ru>
2025-08-30 22:27:29 +0300

    feat(data): add PokeApi interface and Retrofit setup
    Create PokeApi interface with endpoints for pokemon list and details.
    Configure Retrofit with Gson converter and PokeAPI base URL for HTTP
    requests to fetch pokemon data.

---
+++ app/src/main/java/com/example/pokeapi/data/remote/NetworkModule.kt
@@ -0,0 +1,28 @@
+package com.example.pokeapi.data.remote
+
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object NetworkModule {
+    @Provides
+    @Singleton
+    fun provideRetrofit(): Retrofit {
+        return Retrofit.Builder()
+            .baseUrl("https://pokeapi.co/api/v2/")
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+    }
+
+    @Provides
+    @Singleton
+    fun providePokeApi(retrofit: Retrofit): PokeApi {
+        return retrofit.create(PokeApi::class.java)
+    }
+}
