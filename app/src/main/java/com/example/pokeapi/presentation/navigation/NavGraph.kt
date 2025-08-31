676dc828a587e662b38bf110b9e812af2673f5bb
Kris <blackphoenix134@mail.ru>
2025-09-01 01:31:51 +0300

    feat(navigation): refactor to use NavGraph.kt
    Create NavGraph.kt with NavHost for list and detail screens.

--- app/src/main/java/com/example/pokeapi/presentation/navigation/NavGraph.kt
+++ app/src/main/java/com/example/pokeapi/presentation/navigation/NavGraph.kt
@@ -4,28 +4,21 @@ import androidx.compose.runtime.Composable
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
-import com.example.pokeapi.presentation.ui.PokemonDetailScreen
-import com.example.pokeapi.presentation.ui.PokemonFilterScreen
-import com.example.pokeapi.presentation.ui.PokemonListScreen
+import com.example.pokeapi.presentation.ui.screen.PokemonDetailScreen
+import com.example.pokeapi.presentation.ui.screen.PokemonListScreen
 
 @Composable
 fun NavGraph(navController: NavHostController) {
     NavHost(navController = navController, startDestination = "list") {
         composable("list") {
             PokemonListScreen(
-                navigateToDetail = { name -> navController.navigate("detail/$name") },
-                navigateToFilter = { navController.navigate("filter") }
+                navigateToDetail = { name -> navController.navigate("detail/$name") }
             )
         }
         composable("detail/{name}") { backStackEntry ->
             val name = backStackEntry.arguments?.getString("name") ?: ""
             PokemonDetailScreen(
                 name = name,
-                navigateBack = { navController.popBackStack() }
-            )
-        }
-        composable("filter") {
-            PokemonFilterScreen(
                 navigateBack = { navController.popBackStack() }
             )
         }
